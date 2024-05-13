f3e368435f13b45948612d1eed36d046a44f3c7b
seansakumoto <106946168+seansakumoto@users.noreply.github.com>
2024-05-13 13:17:35 +0900

    ゲームオーバーの文字

--- GameOverScene.h
+++ GameOverScene.h
@@ -1,29 +1,29 @@
-#pragma once
-#include"SceneBase.h"
-#include"define.h"
+#include "SceneBase.h"
+#include <string>
 
-class GameOverScene :public SceneBase
+class GameOverScene : public SceneBase
 {
 private:
+    int gameover_image;  // ゲームオーバー画像
+    int transparency;
+    int transparencySpeed;
+    int imagePosX, imagePosY;
 
-	
-	char text[256];
-	bool flag;
-	int FileHandle;
-	int count = 0;
-	int draw_x = 0;
-	int draw_y = 0;
-	
+    // テキスト表示用の変数
+    char textBuffer[256];  // 表示するテキスト
+    int textLength;        // テキストの長さ
+    int textIndex;         // 現在表示しているテキストのインデックス
+    int draw_x, draw_y;    // テキストの描画位置
+
 public:
-	GameOverScene();
-	virtual ~GameOverScene();
+    GameOverScene();
+    virtual ~GameOverScene();
 
-	virtual void Initialize() override;
-	virtual void Finalize() override;
+    virtual void Initialize() override;
+    virtual void Finalize() override;
+    virtual SCENE_TYPE Update(float delta_time) override;
+    virtual void Draw() const override;
 
-	void talk(int x, int y, const char t[], int nCount);
-	virtual SCENE_TYPE Update(float delta_time) override;
-	virtual void Draw() const override;
-
-	virtual SCENE_TYPE GetNowScene() const override;
+    virtual SCENE_TYPE GetNowScene() const override;
+    void LoadText(const char* filePath);  // テキストをファイルから読み込む関数
 };
