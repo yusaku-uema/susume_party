d3f89a181056f74f9e57dac0e6864344bcc52bc1
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2024-05-29 10:11:41 +0900

    爆発アニメーション

--- Attack.cpp
+++ Attack.cpp
@@ -6,6 +6,15 @@ Attack::Attack(class Stage* stage, class PlayerManager* player_manager, class En
 	BoxCollider(location, size),stage(stage), player_manager(player_manager), enemy_manager(enemy_manager),
 duration_time(duration_time),speed(speed),attack_power(attack_power), attack_image(attack_image)
 {
+
+	if (LoadDivGraph("image/Player/explosion.png", 8,8, 1, 48, 48, explosion_image) == -1)throw("image/Player/explosion.pngが読み込めません\n");
+
+	image = explosion_image;
+	image_num = 8;
+	draw_image_num = 0;
+	change_image_time = 0.0f;
+	
+
 	OutputDebugString("Attackコンストラクタ呼ばれました。\n");
 }
 
@@ -19,6 +28,12 @@ bool Attack::Update(float delta_time, bool is_player_attack)
 	location.x += speed.x;
 	location.y += speed.y;
 
+	if ((change_image_time += delta_time) > 0.08)
+	{
+		if (++draw_image_num > image_num - 1)return true;
+		change_image_time = 0.0f;
+	}
+
 	if (stage->HitBlock(this))return true;
 
 	return ((duration_time -= delta_time) < 0.0f);
@@ -31,7 +46,7 @@ void Attack::Draw()const
 	if ((draw_location.x >= -radius.x) && (draw_location.x <= SCREEN_WIDTH + radius.x))
 	{
 		DrawBox(draw_location.x - radius.x, draw_location.y - radius.y, draw_location.x + radius.x, draw_location.y + radius.y, 0xffffff, FALSE);
-		//DrawRotaGraph(draw_location.x, draw_location.y, 1, 0, player_image[0], TRUE);
+		DrawRotaGraph(draw_location.x, draw_location.y, 5, 0, explosion_image[draw_image_num], TRUE);
 	}
 }
 
