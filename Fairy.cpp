d15d98318bd11c35060518b47ad9b92e6e25d990
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2024-06-05 09:28:53 +0900

    コンフリクト解決

--- Fairy.cpp
+++ Fairy.cpp
@@ -231,7 +231,7 @@ void Fairy::Attack()
 			}
 
 			//UŒ‚
-			attack_manager->AddEnemyAttack(location, { 15,15 }, { (dx / distance) * 2.5f ,(dy / distance) * 2.5f }, 10, 3, 1);
+			attack_manager->AddEnemyAttack(location, { 15,15 }, { (dx / distance) * 2.5f ,(dy / distance) * 2.5f }, 10, 3, BIG_EXPLOSION);
 
 			state = FAIRY_STATE::NORMAL;
 		}
