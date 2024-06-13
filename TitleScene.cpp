41c723d549b44ddb4e8c0988bc8e67e1a2d0f183
seansakumoto <106946168+seansakumoto@users.noreply.github.com>
2024-06-13 10:00:48 +0900

    画像の修正とタイトルシーン作成

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -2,8 +2,25 @@
 #include"DxLib.h"
 #include"Key.h"
 
+#define INPUT_ACCEPTANCE_TIME 10
+
 TitleScene::TitleScene()
 {
+	input_time = 0;
+	
+	select_menu = 0;
+	help_menu = 0;
+
+	help=false;
+	credit = false;
+
+	LoadDivGraph("image/title/menu.png", 8, 2, 4, 600, 100, set_menu_image);
+	int count = 0;
+	for (int i = 0; i < MENU_NUM; i++)
+	{
+		for (int j = 0; j < 2; j++)menu_image[i][j] = set_menu_image[count++];
+	}
+
  
 }
 
--- TitleScene.h
+++ TitleScene.h
@@ -1,9 +1,22 @@
 #pragma once
 #include "SceneBase.h"
 
+#define MENU_NUM 4
+
 class TitleScene :public SceneBase
 {
 private:
+	int image;//ƒ^ƒCƒgƒ‹‰æ‘œ
+	int menu_image[MENU_NUM][2];
+	int num_image[4];
+	int set_menu_image[8];
+
+	int input_time;
+	int select_menu;
+	int help_menu;
+
+	bool help;
+	bool credit;
 
 public:
 	TitleScene();
