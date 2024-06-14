ebf8b04f05adace593c800bbaec9e5a250246499
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2024-06-14 14:36:33 +0900

    バードのデバック表示削除

--- Stage.cpp
+++ Stage.cpp
@@ -11,7 +11,7 @@ Stage::Stage(Ui* ui) : camera_work(0.0f), stop_time(0.0f), time_count(0.0f)
 	enemy_manager = new EnemyManager(this, player_manager, attack_manager);
 	enemy_manager->SpawnEnemy(0, { 500,300 }); //敵呼び出し、（呼び出す敵、スポーンさせたい座標）
 
-	for (float i = 0; i < 10; i++)
+	for (float i = 0; i < 5; i++)
 	{
 		enemy_manager->SpawnEnemy(0, { 100 + (i*250),300 });
 	}
